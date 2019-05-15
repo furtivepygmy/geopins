29449c07f744b79b25927d18e8a4d386743ba9d7
Furtive Pygmy <rzvgib001@gmail.com>
2019-05-15 10:54:51 +0400

    First commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+new ApolloServer({
+  typeDefs: '',
+  resolvers: ''
+});
