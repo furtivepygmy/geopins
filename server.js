4a701ec8add2fb1ef7a5b75bee33a5c3600bc5fe
Furtive Pygmy <rzvgib001@gmail.com>
2019-05-15 17:58:13 +0400

    OAuth set up

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
-
-require('dotenv').config();
+const { findOrCreateUser } = require('./controllers/userController');
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
@@ -13,7 +13,21 @@ mongoose
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.log(`Unable to authenticate user with token ${authToken}`);
+    }
+    return { currentUser };
+  }
 });
 
 server.listen().then(({ url }) => {
