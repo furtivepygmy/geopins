092b100ab1e34e74353146273f697ba0244ca5ec
Furtive Pygmy <rzvgib001@gmail.com>
2019-05-15 16:22:04 +0400

    Database set up

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
 
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
-new ApolloServer({
-  typeDefs: '',
-  resolvers: ''
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('Connected to database'))
+  .catch(err => console.error(err));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server is listening on ${url}`);
 });
